c87a72740ff2e2ae08b3a6c51488d35d91ae70be
riyabelle25 <riyaelizabethj@gmail.com>
2022-03-01 01:56:56 +0530

    write repoOSSF results into JSON

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const core = require('@actions/core');
+const { Octokit } = require('@octokit/core');
 // const github = require('@actions/github');
 const { execSync } = require('child_process');
 const fs = require('fs/promises');
@@ -10,11 +11,13 @@ const fs = require('fs/promises');
 const main = async () => {
 	try {
 		const token = core.getInput('token', { required: true }); // to be used when introducing GH Action YAML
+
 		/*
 		 * const {GH_TOKEN} = process.env;
 		 * const token = GH_TOKEN;
 		 */
 
+		const octokit = new Octokit({ auth: token });
 		const getRepos = execSync('npx repo-report ls', {
 			encoding: 'utf-8', env: {
 				...process.env,
@@ -23,7 +26,8 @@ const main = async () => {
 		});
 		const repositories = getRepos.slice(0, getRepos.length - 1).split('\n');
 		const repoOSSF = {};
-		repositories.forEach((repository) => {
+		repositories.reduce(async (prev, repository) => {
+			await prev;
 			console.log(repository);
 			const cmd = `scorecard --repo=github.com/${repository} | grep Aggregate`;
 			const output = execSync(cmd, {
@@ -32,13 +36,21 @@ const main = async () => {
 					GITHUB_AUTH_TOKEN: token,
 				},
 			});
-			repoOSSF[repository] = output.slice(17).replace('\n', '');
+			const getRateLimit = await octokit.request('GET /rate_limit');
+			console.log(getRateLimit.data.rate);
+			const writeOSSF = async () => {
+				repoOSSF[repository] = await output.slice(17).replace('\n', '');
+				console.log('42', repoOSSF);
+			};
+			const callwriteOSSF = await writeOSSF();
+			console.log(callwriteOSSF);
 			console.log('Aggregate score for', repository, ': ', output.slice(17));
+		}, Promise.resolve()).then(() => {
+			console.log('48', repoOSSF);
+			const json = JSON.stringify(repoOSSF, null, 4);
+			const result = fs.writeFile('metadata-ossf-score.json', json, 'utf8');
+			console.log(result);
 		});
-
-		const json = JSON.stringify(repoOSSF, null, 4);
-		const result = await fs.writeFile('repo-report-ossf-score.json', json, 'utf8');
-		console.log(result);
 
 	} catch (error) {
 		core.setFailed(error.message);
